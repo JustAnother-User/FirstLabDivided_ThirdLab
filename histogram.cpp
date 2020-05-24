8d8325b10f8455e3ed4a880c51188d5dc4b6b98a
SazonovPO <>
2020-05-25 02:27:15 +0300

    code: lab03-test - добавление тестов
    Также: Обработка аномалии с пустым массивом
    amend: Сохранение изменений в работе проектов

--- histogram.cpp
+++ histogram.cpp
@@ -3,6 +3,10 @@
 void find_minmax
 (vector<double> numbers, double& min, double& max)
 {
+    if (numbers.empty())
+{
+    cout<<"empty vector";
+}
     min = numbers[0];
     max = numbers[0];
    for (double x : numbers)
